c27ebd9b772d9e8d37ceedd40e19f3c5a763f768
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-10-29 18:31:54 -0600

    Change faceArray to let instead of const

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
     //   rightCol: imageWidth - (clarifaiFace.right_col * imageWidth),
     //   bottomRow: imageHeight - (clarifaiFace.bottom_row * imageHeight),
     // }
-    const faceArray = [];
+    let faceArray = [];
     const clarifaiFacesData = data.outputs[0].data.regions;
     const image = document.getElementById('inputImage');
     const imageWidth = Number(image.width);
