112d590ba07f816761c36032e7f958093ce6c17a
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-10-29 19:24:57 -0600

    Implement multiple boxes, except for not displaying within FaceRecognition Component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageURL, boxes }) => {
-  console.log('From FaceRecognition Component', boxes);
   return(
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputImage' src={imageURL} alt='' width='500px' height='auto'/>
+        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
+        </div>
       </div>
     </div>
   )
